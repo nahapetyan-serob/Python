767ea76455b16c9cae1d642ea31f666919c2b113
Serob7 <124069899+Serob7@users.noreply.github.com>
2023-02-08 23:50:58 +0400

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,8 @@
+print ("Enter your number")
+num = int (input())
+if num == 0:
+	print ("Neither even nor odd")
+elif num % 2 == 1:
+	print ("That is an odd number")
+else:
+	print ("That is an even number")
