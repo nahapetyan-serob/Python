c3f7ab9fbe8478c9d8644f7c601d90188861892f
Serob7 <124069899+Serob7@users.noreply.github.com>
2023-02-09 00:50:10 +0400

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,28 @@
+print ("Please enter a character")
+char = str (input())
+if char == 'A':
+    print ("That is a vowel")
+elif char == 'a':
+    print ("That is a vowel")
+elif char == 'E':
+    print ("That is a vowel")
+elif char == 'e':
+    print ("That is a vowel")
+elif char == 'I':
+    print ("That is a vowel")
+elif char == 'i':
+    print ("That is a vowel")
+elif char == 'O':
+    print ("That is a vowel")
+elif char == 'o':
+    print ("That is a vowel")
+elif char == 'U':
+    print ("That is a vowel")
+elif char == 'u':
+    print ("That is a vowel")
+elif char == 'Y':
+    print ("That is a vowel")
+elif char == 'y':
+    print ("That is a vowel")
+else:
+    print ("That is a consonant")
